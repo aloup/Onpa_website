31ab5bb60985ff641533d1a5e7cc47c86b7faa95
aloup <alexloupova@gmail.com>
2020-11-21 15:43:22 -0500

    added Jolie's code

---
+++ SQL/INFO FOR TABLES.sql
@@ -0,0 +1,5 @@
+INSERT INTO ARTIST
+	VALUES	('A1', 'Billie', 'Eilish', '2001-12-18', 'https://www.facebook.com/billieeilish', 'https://www.instagram.com/billieeilish/', 'https://twitter.com/billieeilish', FALSE, TRUE, FALSE, TRUE, FALSE, 'D3', 'G5', 'Soprano'),
+			('A2', 'Finneas', 'O'Connell', '1997-07-30', 'https://www.facebook.com/FINNEASOFFICIAL', 'https://www.instagram.com/finneas/', 'https://twitter.com/finneas', TRUE, TRUE, FALSE, TRUE, FALSE, 'E2', 'F5', 'Baritone'),
+			('A3', 'Stefani', 'Germanotta', '1986-03-28', 'https://www.facebook.com/ladygaga', 'https://www.instagram.com/ladygaga/', 'https://twitter.com/ladygaga', TRUE,	TRUE, FALSE, TRUE, FALSE, 'F#2', 'C6', 'Lyric Mezzo-Soprano');
+	
