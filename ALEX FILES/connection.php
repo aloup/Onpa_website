5bd74dc256f8acf9d4cc3c0f4fd37e2e590a8786
aloup <alexloupova@gmail.com>
2020-12-13 20:42:07 -0500

    file reorg

---
+++ ALEX FILES/connection.php
@@ -0,0 +1,6 @@
+<?php
+	$servername = "localhost";
+	$server_username = "alexandraloupova";
+	$server_password = "***********"; // you're going to need to add your own credentials where it says alexloupova and your own password
+	$dbname = "alexandraloupova";
+?>
--- connection - Copy.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$servername = "localhost";
-	$server_username = "alexandraloupova";
-	$server_password = "***********"; // you're going to need to add your own credentials where it says alexloupova and your own password
-	$dbname = "alexandraloupova";
-?>
